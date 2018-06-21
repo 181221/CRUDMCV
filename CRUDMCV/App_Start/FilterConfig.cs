2d3d890aaa04badb1507189ce1e483c1843a35e8
Peder <peder.wiig@gmail.com>
2018-06-21 21:34:27 +0200

    Add project files.

---
+++ CRUDMCV/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDMCV
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
